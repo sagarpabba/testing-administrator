1470863fb39d3eada3106a3521f928de4c50928d
alterhu2020@gmail.com <alterhu2020@gmail.com@532e9711-d973-b06a-7f70-5a9e946ec87e>
2014-02-18 07:16:33 +0000

    20140218 Alter:the bundle string get method changed.

--- TA/src/com/hp/utility/Bundle.java
+++ TA/src/com/hp/utility/Bundle.java
@@ -1,15 +1,66 @@
 package com.hp.utility;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Bundle{
-	   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("settings"); //resource.properties file
-	   public static String getString(String key) {
+	    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("settings"); //resource.properties file
+	  
+	    private static ResourceBundle RESOURCE_BUNDLE_ABSOLUTEPATH=null;
+	    static{
+	    	 File file = new File(""); //any parent path you specified here
+	    	 try {
+		    		 URL[]  urls={file.toURI().toURL()};
+		    		 ClassLoader loader = new URLClassLoader(urls);
+			    	 RESOURCE_BUNDLE_ABSOLUTEPATH = ResourceBundle.getBundle("settings", Locale.getDefault(), loader);
+	    		 } 
+	    	 catch (MalformedURLException e){
+			        e.printStackTrace();
+			 }	    	
+	     }
+	  
+	   
+	   /** 
+	* @Title: getClassPathString 
+	* @Description: TODO
+	* @author alterhu2020@gmail.com
+	* @param @param key
+	* @param @return    
+	* @return String    return type
+	* @throws 
+	*/ 
+	
+	public static String getClassPathString(String key) {
 	     try {
 	        return RESOURCE_BUNDLE.getString(key);
 	     } catch (MissingResourceException e) {
 	       return '!' + key + '!'; 
-	     }
+	     }	 
 	   }
+	
+	
+	   /** 
+	* @Title: getAbsolutePathString 
+	* @Description: TODO
+	* @author alterhu2020@gmail.com
+	* @param @param key
+	* @param @return    
+	* @return String    return type
+	* @throws 
+	*/ 
+	
+	public static String getAbsolutePathString(String key) {
+		     try {
+		        return RESOURCE_BUNDLE_ABSOLUTEPATH.getString(key);
+		     } catch (MissingResourceException e) {
+		       return '!' + key + '!'; 
+		     }
+		     
+		 
+		   }
 	}
